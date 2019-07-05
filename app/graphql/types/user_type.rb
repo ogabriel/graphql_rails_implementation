d4a482ba9b1b5b001d26dc577422b701648f0f04
ogabriel <gabrieloliver8991@gmail.com>
2019-07-04 23:01:36 -0300

    add graphql: user object

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class UserType < Types::BaseObject
+    description 'User'
+
+    field :id, String, null: false
+    field :email, String, null: false
+    # field :password, String, null: false
+    # field :quotes, [Types::QuoteType]
+  end
+end
