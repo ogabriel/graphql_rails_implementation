a15683ebe6e7608d6fca756dd8ecf5983ce45a1e
ogabriel <gabrieloliver8991@gmail.com>
2019-07-06 19:16:01 -0300

    update update quote

--- app/graphql/mutations/update_quote.rb
+++ app/graphql/mutations/update_quote.rb
@@ -5,18 +5,30 @@ module Mutations
     graphql_name 'UpdateQuote'
 
     field :quote, Types::QuoteType, null: true
-    field :result, Boolean, null: true
+    field :successful, Boolean, null: true
 
     argument :id, String, required: true
-    argument :message, String, required: true
+    argument :userPassword, String, required: true
+    argument :message, String, required: false
+    argument :newUserId, String, required: false
 
     def resolve(**args)
-      quote = Quote.find(args[:id])
-      quote.update(message: args[:message])
+      quote = Quote.includes(:user).find(args[:id])
+      user = quote.user
+      if user.valid_password?(args[:user_password])
+        update_handler(quote, args)
+      else
+        quote.errors.add(:password, 'password not valid')
+      end
       {
         quote: quote,
-        result: quote.errors.blank?
+        successful: quote.errors.blank?
       }
+    end
+
+    def update_handler(quote, args)
+      quote.update(message: args[:message]) if args[:message]
+      quote.update(user: User.find(args[:new_user_id])) if args[:new_user_id]
     end
   end
 end
