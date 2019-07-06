de488deeef4818c58bf937f954f98a00fa7c843a
ogabriel <gabrieloliver8991@gmail.com>
2019-07-06 18:21:08 -0300

    updating description on types

--- app/graphql/types/quote_type.rb
+++ app/graphql/types/quote_type.rb
@@ -2,6 +2,8 @@
 
 module Types
   class QuoteType < Types::BaseObject
+    description 'This a quote from the most interesting man in the world'
+
     field :id, String, null: false
     field :email, String, null: true
     field :message, String, null: true
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,11 +2,10 @@
 
 module Types
   class UserType < Types::BaseObject
-    description 'User'
+    description 'a user that may know, or may not know'
 
     field :id, String, null: false
     field :email, String, null: false
-    # field :password, String, null: false
     field :quotes, [Types::QuoteType], null: false
   end
 end
